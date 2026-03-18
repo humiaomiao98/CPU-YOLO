6e7467472564d60b4a057e0cc5c24bd0c427f151
humiaomiao98 <1821783386@qq.com>
2026-03-18 10:00:14 +0800

    Update detect.py

--- tools/detect.py
+++ tools/detect.py
@@ -5,8 +5,8 @@ from ultralytics import YOLO
 if __name__ == '__main__':
 
     # Load a model
-    model = YOLO(model=r'H:\ultralytics-main\runs\detect\runs\train\CPU-YOLO\weights\best.pt')
-    model.predict(source=r'H:\ultralytics-main\Datasets\test\images',
+    model = YOLO(model=r'H:\ultralytics-xx\runs\detect\runs\train\CPU-YOLO\weights\best.pt')
+    model.predict(source=r'H:\ultralytics-xx\Datasets\test\images',
                   save=True,
                   #show=True,
                   )
